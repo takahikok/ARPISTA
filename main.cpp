de88be49fc7e315d0797adba2cc36b25df540743
Kobayashi Takahiko <takahiko_k@live.jp>
2017-09-18 20:56:33 +0900

    git test

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	w.Init(&settings);
 	w.show();
 
-	TKQXMLUtility::SaveXMLFile("C:\\Users\\user\\Documents\\ARPISTA\\settings_out.xml",
+	TKQXMLUtility::SaveXMLFile("C:\\Users\\user\\Documents\\ARPISTA\\settings_gitignore.xml",
 			      doc);
 	return a.exec();
 }
