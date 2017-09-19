d005528bbb6e80e8b831d6b6fe7a799b9fc33de4
Kobayashi Takahiko <takahiko_k@live.jp>
2017-09-19 09:17:32 +0900

    Opera style MDI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent) :
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+	ui->mdiArea->setOption(QMdiArea::DontMaximizeSubWindowOnActivation);
 }
 
 MainWindow::~MainWindow()
